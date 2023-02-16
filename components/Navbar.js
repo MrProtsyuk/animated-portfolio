6983c08f3b4cac02a62d42d2149f1aadeb9d408c
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2023-02-16 13:25:29 -0800

    added nav bar with functionality

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,49 @@
 import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  const router = useRouter();
+  return (
+    <div className="flex text-lg md:text-2xl lg:text-3xl 2xl:text-7xl font-medium max-w-[1536px] justify-center md:justify-between items-center z-20">
+      <ul className="flex gap-4 md:gap-12">
+        <Link href="/">
+          <li
+            className={`cursor-pointer ${
+              router.pathname == "/" ? "underline" : ""
+            }`}
+          >
+            Home
+          </li>
+        </Link>
+        <Link href="/about">
+          <li
+            className={`cursor-pointer ${
+              router.pathname == "/about" ? "underline" : ""
+            }`}
+          >
+            About
+          </li>
+        </Link>
+        <Link href="/projects">
+          <li
+            className={`cursor-pointer ${
+              router.pathname == "/projects" ? "underline" : ""
+            }`}
+          >
+            Projects
+          </li>
+        </Link>
+        <Link href="/contact">
+          <li
+            className={`cursor-pointer ${
+              router.pathname == "/contact" ? "underline" : ""
+            }`}
+          >
+            Contact
+          </li>
+        </Link>
+      </ul>
+    </div>
+  );
 }
