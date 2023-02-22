ecd1c488066d37da1d1afef29cf238048138c5ff
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2023-02-21 17:22:50 -0800

    changes

--- pages/about.js
+++ pages/about.js
@@ -26,28 +26,20 @@ export default function about() {
         variants={container}
         initial="hidden"
         animate="show"
-        className="flex flex-col justify-center items-center w-72 sm:w-auto bg-slate-200 border rounded-sm p-16 mb-12 text-slate-700"
+        className="flex flex-col w-72 sm:w-auto bg-slate-200 border rounded-sm p-16 mb-12 text-slate-700"
       >
-        <div className="flex flex-col lg:flex-row justify-center items-center lg:items-start">
+        <div className="flex flex-col lg:flex-row justify-center items-center">
           <div className="overflow-hidden px-1">
-            <m.div
-              variants={item}
-              className="m-2 w-72 sm:w-80 2xl:w-2/12 border border-transparent rounded-md overflow-hidden"
-            >
-              <Image
-                width={400}
-                height={200}
-                src="/images/coverpic.jpeg"
-                alt="mark protsyuk cover image"
-                className=""
-              ></Image>
-            </m.div>
+            <Image
+              width={400}
+              height={200}
+              src="/images/coverpic.jpeg"
+              alt="mark protsyuk cover image"
+              className="w-80 sm:w-80 2xl:w-2/12 border border-transparent rounded-md"
+            ></Image>
           </div>
-          <div className="overflow-hidden py-1">
-            <m.p
-              variants={item}
-              className="text-center lg:text-right text-lg lg:text-2xl m-2 w-72 sm:w-8/12 border border-transparent rounded-md"
-            >
+          <div className="flex overflow-hidden py-1 items-center justify-center">
+            <p className="text-center lg:text-right text-lg lg:text-xl m-2 w-72 sm:w-80 xl:w-96 2xl:w-auto border border-transparent rounded-md">
               Hello! My name is Mark Protsyuk and I am a Software Engineer from
               Sacramento, CA! I am a recent UC Davis Bootcamp graduate. I have
               had some local intern experience and now I am excited to work in a
@@ -56,29 +48,21 @@ export default function about() {
               are snowboarding, playing music, and climbing. These activities
               really help me to push myself into trying new things regardless of
               the difficulty I may encounter.
-            </m.p>
+            </p>
           </div>
         </div>
-        <div className="flex flex-col lg:flex-row justify-center items-center lg:items-start">
-          <div className="">
-            <m.div
-              variants={item}
-              className="w-72 sm:w-80 2xl:w-2/12 border border-transparent rounded-md overflow-hidden"
-            >
-              <Image
-                width={400}
-                height={200}
-                src="/images/daviscertification.png"
-                alt="mark protsyuk cover image"
-                className=""
-              />
-            </m.div>
-          </div>
+        <div className="flex flex-col m-4 lg:flex-row justify-center items-center lg:items-start">
           <div className="overflow-hidden py-1">
-            <m.p
-              variants={item}
-              className="text-center lg:text-right text-xl lg:text-2xl m-2 w-72 sm:w-8/12 border border-transparent rounded-md"
-            >
+            <Image
+              width={400}
+              height={200}
+              src="/images/daviscertification.png"
+              alt="mark protsyuk cover image"
+              className="w-72 sm:w-80 2xl:w-2/12 border border-transparent rounded-md "
+            />
+          </div>
+          <div className="flex justify-center items-center overflow-hidden py-1">
+            <p className="text-center lg:text-right text-xl lg:text-xl m-2 w-72 sm:w-80 xl:w-96 2xl:w-auto border border-transparent rounded-md">
               As stated above, I recently graduatated from the UC Davis Coding
               Bootcamp as of June 2022. I have also completed a course provided
               by Meta on Front-End Development. Besides courses, I also have
@@ -86,7 +70,7 @@ export default function about() {
               Sacramento, CA called Dev Catapult. Working for Dev Catapult has
               taught me many things and is where I learned to sharpend my React
               skills.
-            </m.p>
+            </p>
           </div>
         </div>
       </m.div>
--- pages/contact.js
+++ pages/contact.js
@@ -9,7 +9,43 @@ export default function contact() {
       exit={{ opacity: 1 }}
       initial={{ y: "100%" }}
       transition={{ duration: 0.75, ease: "easeOut" }}
-      className="absolute top-0 left-0 w-full h-full bg-slate-900 lg:px-48 px-16 "
-    ></m.div>
+      className="absolute top-0 left-0 w-full h-full bg-slate-900 lg:px-48 px-16 justify-center items-center"
+    >
+      <m.div
+        variants={container}
+        initial="hidden"
+        animate="show"
+        className="flex justify-center items-center w-80 sm:w-4/6 bg-slate-200 border rounded-sm p-4 mt-40 mb-12 text-slate-700"
+      >
+        <form className="flex flex-col overflow-hidden px-1">
+          <m.label className="flex flex-col p-4 m-4 w-auto" variants={item}>
+            Name:
+            <input placeholder="Your Name!"></input>
+          </m.label>
+          <m.label
+            className="flex flex-col p-4 m-4 w-72 sm:w-5/12"
+            variants={item}
+          >
+            Email:
+            <input placeholder="Your Email"></input>
+          </m.label>
+          <m.label
+            className="flex flex-col p-4 m-4 w-72 sm:w-5/12"
+            variants={item}
+          >
+            Message:
+            <textarea placeholder="Leave a message!"></textarea>
+          </m.label>
+          <m.div className=" justify-center">
+            <button
+              className="p-4 m-2 bg-slate-700 text-white hover:bg-gray-500"
+              variants={item}
+            >
+              Send!
+            </button>
+          </m.div>
+        </form>
+      </m.div>
+    </m.div>
   );
 }
